1598276baf2d494d9505d159a2f63cc982ac5b5b
mariano rodo <cmrs200958@gmail.com>
2023-11-23 20:20:55 -0300

    incorporacion server

--- server/index.js
+++ server/index.js
@@ -1,1 +1,27 @@
-console.log("hola de index")
+import net from "node:net"
+import dotenv from "dotenv"
+
+dotenv.config()
+
+const port=process.env.PORT ?? 2323 // esto cuando me olvido de definir en env el puerto, me lo toma autom
+
+
+const serverTCP=net.createServer()
+
+serverTCP.on("connection",(socket)=>{
+    socket.on("data",()=>{
+
+    })
+    socket.on("cloes",()=>{
+        console.log("client disconnected")
+    })
+    socket.on("error",()=>{
+        console.log("client error")
+    })
+
+    console.log("client connected", new Date().toLocaleString())
+})
+
+serverTCP.listen(port,()=>{
+    console.log("server is up ", port, new Date().toLocaleString())
+})
